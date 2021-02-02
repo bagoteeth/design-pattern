c03b5317098f5caa8b636875490e6b9ce4402564
Jiang ruofa <ruofa.jiang@liangyihui.net>
2021-02-02 10:09:25 +0800

    mvc

--- src/com/jrf/designPattern/MVC/Test.java
+++ src/com/jrf/designPattern/MVC/Test.java
@@ -5,4 +5,20 @@ package com.jrf.designPattern.MVC;
  * @Date 2021/2/1 18:41
  */
 public class Test {
+    public static void main(String[] args) {
+        Student student=getFromDB();
+        StudentView studentView=new StudentView();
+
+        StudentController studentController=new StudentController(student,studentView);
+        studentController.updateView();
+        studentController.setName("bago");
+        studentController.updateView();
+    }
+    public static Student getFromDB(){
+        Student student=new Student();
+        student.setId(1);
+        student.setName("teeth");
+        student.setAge(20);
+        return student;
+    }
 }
