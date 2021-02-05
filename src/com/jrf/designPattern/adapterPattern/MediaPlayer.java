51cbf5acc694e117ec346b562820f2264f9b1dba
Jiang ruofa <ruofa.jiang@liangyihui.net>
2021-02-05 18:23:06 +0800

    适配器模式

---
+++ src/com/jrf/designPattern/adapterPattern/MediaPlayer.java
@@ -0,0 +1,9 @@
+package com.jrf.designPattern.adapterPattern;
+
+/**
+ * @Author ruofa
+ * @Date 2021/2/5 18:21
+ */
+public interface MediaPlayer {
+    void play(String audioType,String fileName);
+}
